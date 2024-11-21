9e9d09f453b512b14474c87f765515b1d6dce39c
frn-af <91farhanalfathra@gmai.com>
2024-11-21 13:24:57 +0700

    add back cava
    bring back cava that causes some problem

--- hydenix/packages/music.nix
+++ hydenix/packages/music.nix
@@ -3,6 +3,7 @@
 {
   home.packages = with pkgs; [
     # --------------------------------------------------- // Music
+    cava
     spicetify-cli # cli to customize spotify client
     spotify # spotify client
   ];
